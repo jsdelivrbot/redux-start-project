6e1ded85b5273b4925d5b0e67a25a17e13dcb5ce
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-20 23:24:17 +0100

    Create components structure and start coding the Search bar lass-component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+/*API Key from https://console.developers.google.com/apis/dashboard for YouTube*/
+const API_KEY = "AIzaSyCF_ikzfUXduzWPsg3EnBR_1zmnxra0i_8";
+// Create a new component. This component should produce some HTML.
+const App = () => {
+	return(
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Put component into the DOM, inside the element with a class of "container"
+ReactDOM.render(<App />, document.querySelector('.container'));
