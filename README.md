980166488f268cb63f2874dbafaa47c83019620e
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-21 19:39:28 +0100

    Fetching videos, begin build list.

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# ReduxSimpleStarter
+# Project based on on ReduxSimpleStarter
+This project is based on ReduxSimpleStarter, following the course [Redux](https://www.udemy.com/react-redux/).
+See below for info on the [course](https://www.udemy.com/react-redux/) and on [ReduxSimpleStarter](https://github.com/StephenGrider/ReduxSimpleStarter).
+
+=============================================================
+
+## ReduxSimpleStarter
 
 Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
-###Getting Started###
+### Getting Started
 
 There are two methods for getting started with this repo.
 
-####Familiar with Git?#####
+#### Familiar with Git
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
@@ -16,7 +22,7 @@ Checkout this repo, install dependencies, then start the gulp process with the f
 	> npm start
 ```
 
-####Not Familiar with Git?#####
+#### Not Familiar with Git
 Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
 
 ```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-
-/*API Key from https://console.developers.google.com/apis/dashboard for YouTube*/
+/* Packages */
+import React, { Component } 	from "react";
+import ReactDOM 					from "react-dom";
+import YTSearch 					from "youtube-api-search";
+/* Components */
+import SearchBar 					from "./components/search_bar";
+import VideoList					from "./components/video_list";
+/* API Key from https://console.developers.google.com/apis/dashboard for YouTube */
 const API_KEY = "AIzaSyCF_ikzfUXduzWPsg3EnBR_1zmnxra0i_8";
-// Create a new component. This component should produce some HTML.
-const App = () => {
-	return(
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
 
-//Put component into the DOM, inside the element with a class of "container"
-ReactDOM.render(<App />, document.querySelector('.container'));
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+
+			//By using arrow-function we can use "this". When the key and value is
+			//the same, we can shorten it
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return(
+			<div>
+				<SearchBar />
+				<VideoList videos={ this.state.videos }/>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
