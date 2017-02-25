bcbf8081c14c02a45472eb5e9594e8fde2d66563
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-25 17:07:26 +0100

    Basic presentation functionality

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class App extends Component {
 				<SearchBar />
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList
-					onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+					onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
 					videos={this.state.videos}/>
 			</div>
 		);
